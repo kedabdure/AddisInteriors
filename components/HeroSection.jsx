d46c7102926039bad8fe95a2ed284f4ac1985e56
kedabudre <abdurehimk77@gmail.com>
2024-12-23 09:30:27 +0300

    update the hero section and the 3d model responsiveness

--- components/HeroSection.jsx
+++ components/HeroSection.jsx
@@ -35,14 +35,14 @@ export default function HeroSection() {
   const sizes = calculateSizes(isSmall, isMobile, isTablet);
 
   return (
-    <div className="relative flex flex-col lg:flex-row items-center justify-between h-screen px-5 lg:px-20">
+    <div className="relative flex flex-col lg:flex-row items-center justify-between h-screen px-5 sm:px-14 md:px-32 lg:px-5 xl:px-20">
       <div className='absolute inset-0 z-[-1]'>
         <img src="/hero2.jpeg" alt="hero image" className='w-full h-full object-cover brightness-75' />
       </div>
 
       {/* Left Content: Navigation + Hero Text */}
       <div className="w-full h-full flex flex-col lg:w-1/2 text-center lg:text-left mt-28 lg:mt-0">
-        <nav className="hidden justify-center lg:justify-start gap-6 mb-20 mt-10 md:flex">
+        <nav className="hidden justify-center lg:justify-start gap-6 mb-20 mt-10 lg:flex">
           {navItems.map(({ href, label }) => (
             <Link
               key={href}
@@ -68,17 +68,17 @@ export default function HeroSection() {
           </p>
 
           {/* Buttons */}
-          <div className="flex flex-col gap-2 md:gap-4 md:flex-row justify-center lg:justify-start mt-0 md:mt-24">
+          <div className="flex flex-col gap-3 md:gap-4 md:flex-row justify-center lg:justify-start mt-0 md:mt-5 lg:mt-24">
             <Link
               href="/contact"
-              className="inline-flex mx-auto sm:mx-0 max-w-[170px] items-center px-6 py-2.5 md:px-6 md:py-3 font-medium bg-white text-black border border-transparent rounded-full shadow-md transition-transform duration-300 ease-in-out hover:scale-105"
+              className="inline-flex mx-auto md:mx-0 max-w-[170px] items-center px-6 py-2.5 md:px-6 md:py-3 font-medium bg-white text-black border border-transparent rounded-full shadow-md transition-transform duration-300 ease-in-out hover:scale-105"
             >
               Contact Us
               <TbArrowUpRight className="w-5 h-5 ml-2" />
             </Link>
             <Link
               href="/projects"
-              className="inline-flex mx-auto sm:mx-0 max-w-[170px] items-center px-6 py-2 md:px-6 md:py-3 font-medium text-white border border-white rounded-full shadow-md transition-transform duration-300 ease-in-out hover:bg-gray-950 hover:text-white hover:scale-105"
+              className="inline-flex mx-auto md:mx-0 max-w-[170px] items-center px-6 py-2 md:px-6 md:py-3 font-medium text-white border border-white rounded-full shadow-md transition-transform duration-300 ease-in-out hover:bg-gray-950 hover:text-white hover:scale-105"
             >
               Our Works
               <TbArrowUpRight className="w-5 h-5 ml-2" />
@@ -88,7 +88,7 @@ export default function HeroSection() {
       </div>
 
       {/* Right Content: 3D Model */}
-      <div className="w-full lg:w-[700px] h-[600px] lg:h-full flex items-center justify-center">
+      <div className="w-full lg:w-[550px] xl:w-[670px] h-[600px] md:h-[300px] lg:h-full flex items-center justify-center">
         <Canvas className="w-full h-full">
           <ambientLight intensity={1} />
           <directionalLight intensity={4} position={[-10, 10, 5]} />
@@ -106,9 +106,9 @@ export default function HeroSection() {
             </group>
             <group>
               <LampModel
-                position={[-2, 3.2, -4]}
+                position={sizes.lampPosition}
                 rotation={[0, 0, 0]}
-                scale={5}
+                scale={sizes.lampScale}
                 autoRotate
               />
               <PalmPlantModel
--- constants/index.js
+++ constants/index.js
@@ -1,15 +1,18 @@
 export const calculateSizes = (isSmall, isMobile, isTablet) => {
   return {
-    sofaScale: isSmall ? 9.5 : isMobile ? 10 : isTablet ? 4.5 : 5,
-    sofaRotation: isMobile ? [.19, 0, 0] : [-0.19, -0.55, -.01],
-    sofaPosition: isSmall ? [0, 0.8, 0] : isMobile ? [0, 5, 0] : isTablet ? [7.5, -5, 0] : [0.5, -5, -0.5],
+    sofaScale: isSmall ? 7 : isMobile ? 8 : isTablet ? 9 : 5,
+    sofaRotation: isMobile ? [.1, 0, 0] : isMobile ? [.1, 0, 0] : isTablet ? [.1, 0, 0] : [-0.19, -0.55, -.01],
+    sofaPosition: isSmall ? [0, 0.8, 0.5] : isMobile ? [0, 0.6, 0] : isTablet ? [0, 0.8, 2] : [0.5, -5, -0.5],
 
-    plantScale: isSmall ? 7 : isMobile ? 10 : isTablet ? 4.5 : 5,
+    plantScale: isSmall ? 5.5 : isMobile ? 6 : isTablet ? 7.5 : 4.5,
     plantRotation: isSmall ? [-0.1, 1, 0] : [-0.15, 1, 0],
-    plantPosition: isSmall ? [6.5, 0, 8] : isMobile ? [0, 0, 0] : [5.5, -2.5, 7],
+    plantPosition: isSmall ? [5, 0, 7] : isMobile ? [7.5, 0, 6.5] : isTablet ? [9, 0, 9] : [5, -2.5, 7],
 
-    carpetScale: isSmall ? 12.8 : isMobile ? 11 : 10,
-    carpetPosition: isSmall ? [0, 0, 3] : isMobile ? [7, 0, -5] : isTablet ? [7.5, -5, 0] : [.05, -7, -5],
-    carpetRotation: isSmall ? [0.01, 0, 0] : isMobile ? [0, 0, 0] : [-0.2, -0.5, -0.01],
+    carpetScale: isSmall ? 11 : isMobile ? 13 : isTablet ? 14 : 10,
+    carpetRotation: isSmall ? [0.01, 0, 0] : isMobile ? [0.01, 0, 0] : isTablet ? [0, 0, 0] : [-0.2, -0.5, -0.01],
+    carpetPosition: isSmall ? [0, 0, 3] : isMobile ? [0, 0, 2] : isTablet ? [0, 0, 5] : [.05, -7, -5],
+
+    lampScale: isSmall ? 0 : isMobile ? 0 : isTablet ? 0 : 3.5,
+    lampPosition: isTablet ? [-2, 3.2, -4] : [-2, 7.5, -4],
   };
 };
