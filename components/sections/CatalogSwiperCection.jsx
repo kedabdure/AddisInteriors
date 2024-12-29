58dba851e1d93c234bee6c4ca92bc073ddef7b69
kedabudre <abdurehimk77@gmail.com>
2024-12-29 15:14:05 +0300

    update the swipper section

--- components/sections/CatalogSwiperCection.jsx
+++ components/sections/CatalogSwiperCection.jsx
@@ -4,66 +4,75 @@ import { TbArrowUpRight } from "react-icons/tb";
 import 'swiper/css';
 import { Autoplay } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
+import Link from 'next/link'
 
 
 export default function CatalogSwiperSection() {
+  const settings = {
+    slidesPerView: 1,
+    breakpoints: {
+      640: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      1024: {
+        slidesPerView: 3,
+        spaceBetween: 50,
+      },
+    },
+    autoplay: { delay: 5000, disableOnInteraction: false },
+    modules: [Autoplay],
+  }
 
-    return (
-        <div className="py-8 lg:py-28">
-            <div className="container grid pb-8 lg:grid-cols-2">
-                <div className="text-left ">
-                    <h1 className="py-4 text-4xl font-medium lg:text-6xl lg:py-0">Modern Classic</h1>
-                </div>
-                <div>
-                    <h2 className="pb-6 text-xl font-bold tracking-wider">LUXURY DECOR TO CREATE COMFORT IN OUR HOME</h2>
-                    <div className="grid grid-cols-2 text-gray-500 gap-x-8">
-                        <div>
-                            <p>
-                                With our passion for design and decor, we have created a collection of furniture and accessories that will help you create a comfortable and stylish home.
-                            </p>
-                        </div>
-                        <div>
-                            <p>
-                                We create distinctive and unique designs that are inspired by the latest trends in interior design and fashion.
-                            </p>
-                            <a href="" className="inline-flex items-center pt-4 text-lg font-bold text-black underline">View Gallery <TbArrowUpRight /> </a>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <div className="c-space">
+      <div className="flex flex-col gap-5">
+        <div className="flex items-center justify-between gap-5">
+          <div className="max-w-[500px]">
+            <div className="flex justify-left sm:justify-normal gap-4 items-center mb-8 md:mb-10">
+              <div className="w-12 md:w-16 lg:w-20 h-[3px] bg-black"></div>
+              <h1 className="title-text">Our Products</h1>
             </div>
-
-            <Swiper
-                slidesPerView={1}
-                breakpoints={{
-                    640: {
-                        slidesPerView: 2,
-                        spaceBetween: 20,
-                    },
-                    1024: {
-                        slidesPerView: 3,
-                        spaceBetween: 50,
-                    },
-                }}
-                autoplay={{ delay: 2500, disableOnInteraction: false }}
-                modules={[Autoplay]}
-
+            <div className="flex flex-col gap-3">
+              <h2 className="text-2xl lg:text-2xl font-semibold tracking-normal text-gray-700">
+                Luxury Decor to Create Comfort in Your Home
+              </h2>
+              <p className="text-gray-700 leading-relaxed">
+                Our distinctive and unique designs are inspired by the latest trends in interior design and fashion.
+              </p>
+            </div>
+          </div>
+          <div className="max-w-[200px]">
+            <Link
+              href="/contact"
+              className="inline-flex items-center px-5 py-2 md:px-7 md:py-3 text-md font-medium bg-black text-white border border-transparent rounded-sm shadow-md transition-transform duration-300 ease-in-out hover:scale-105"
             >
-                <SwiperSlide>
-                    <Image src="/image/swiper1.jpg" alt="LOGO" width={520} height={220} className="w-full" />
-                </SwiperSlide>
-                <SwiperSlide>
-                    <Image src="/image/swiper2.jpg" alt="LOGO" width={520} height={220} className="w-full" />
-                </SwiperSlide>
-                <SwiperSlide>
-                    <Image src="/image/swiper3.jpg" alt="LOGO" width={520} height={220} className="w-full" />
-                </SwiperSlide>
-                <SwiperSlide>
-                    <Image src="/image/swiper4.jpg" alt="LOGO" width={520} height={220} className="w-full" />
-                </SwiperSlide>
-                <SwiperSlide>
-                    <Image src="/image/swiper5.jpg" alt="LOGO" width={520} height={220} className="w-full" />
-                </SwiperSlide>
-            </Swiper>
+              Contact Us
+              <TbArrowUpRight className="w-6 h-6 ml-2" />
+            </Link>
+          </div>
         </div>
-    )
+      </div>
+
+      <div className="w-full mt-10">
+        <Swiper {...settings}>
+          <SwiperSlide>
+            <Image src="/image/swiper1.jpg" alt="LOGO" width={520} height={220} className="w-full md:w-[90%]" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Image src="/image/swiper2.jpg" alt="LOGO" width={520} height={220} className="w-full" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Image src="/image/swiper3.jpg" alt="LOGO" width={520} height={220} className="w-full" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Image src="/image/swiper4.jpg" alt="LOGO" width={520} height={220} className="w-full" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Image src="/image/swiper5.jpg" alt="LOGO" width={520} height={220} className="w-full" />
+          </SwiperSlide>
+        </Swiper>
+      </div>
+    </div>
+  )
 }
