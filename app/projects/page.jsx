b758bfa56fa30dd0097cf112241eedbe49202a64
kedabudre <abdurehimk77@gmail.com>
2025-01-01 15:16:59 +0300

    updta the animation of the project section

--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -3,30 +3,18 @@
 import Image from "next/image";
 import { projects } from "@/constants";
 import Button from "@/components/Button";
-import { motion } from "framer-motion";
+import { motion } from 'framer-motion';
+import { motionTextProps, motionImageProps, motionBoxProps } from '@/lib/motionLib';
+
 
 export default function Projects() {
-  const sectionVariants = {
-    hidden: { opacity: 0, y: 50 },
-    visible: {
-      opacity: 1,
-      y: 0,
-      transition: {
-        duration: 1,
-        ease: 'easeOut'
-      }
-    },
-    exit: { opacity: 0, y: 50 }
-  }
 
   return (
     <div>
       {/* Hero Section */}
       <div className="bg-[url('/image/backgroundproject.jpg')] bg-center bg-cover">
         <motion.h1 className="container mx-auto px-4 py-48 mt-12 text-center text-4xl font-extrabold tracking-widest text-white sm:text-5xl md:text-6xl lg:py-72"
-          initial={{ opacity: 0, y: 50 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ delay: 0.3, duration: 1 }}
+          {...motionTextProps}
         >
           Our Projects
         </motion.h1>
@@ -37,9 +25,7 @@ export default function Projects() {
         <div className="mx-auto grid grid-cols-1 gap-6 sm:grid-cols-2 md:gap-8 lg:grid-cols-3">
           {projects.map((project) => (
             <motion.div
-              initial="hidden"
-              whileInView="visible"
-              variants={sectionVariants}
+              {...motionBoxProps}
               key={project.id}
               className="relative overflow-hidden rounded-xl group shadow-md hover:shadow-lg transition-shadow duration-300"
             >
@@ -68,14 +54,14 @@ export default function Projects() {
         {/* Call to Action */}
         <motion.div
           className="relative bg-gradient-to-r from-purple-200/30 via-pink-100/20 to-indigo-300/30 backdrop-blur-xl text-center c-space mt-8 md:mt-16 flex flex-col gap-5 md:gap-8 rounded-md shadow-lg"
-          initial="hidden"
-          whileInView="visible"
-          variants={sectionVariants}
+          {...motionBoxProps}
         >
-          <h2 className="text-xl font-semibold md:text-4xl text-gray-900">
+          <motion.h2 {...motionTextProps} className="text-xl font-semibold md:text-4xl text-gray-900">
             Want to know more about our projects?
-          </h2>
-          <Button href={'/contact'} title={'Contact Us'} />
+          </motion.h2>
+          <motion.div {...motionBoxProps}>
+            <Button href={'/contact'} title={'Contact Us'} />
+          </motion.div>
         </motion.div>
       </div>
     </div>
