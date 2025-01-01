d6ff858580f41da7cd7b06a413ae9279157e56f6
kedabudre <abdurehimk77@gmail.com>
2025-01-01 12:08:55 +0300

    update the about us section

--- components/hero/AboutComponent.jsx
+++ components/hero/AboutComponent.jsx
@@ -8,9 +8,9 @@ import { motionImageProps, motionTextProps, motionBoxProps } from "@/lib/motionL
 export default function AboutComponent() {
 
   return (
-    <div className="container py-16">
+    <div className="c-space">
       <motion.div className="w-full flex flex-col md:flex-row justify-between gap-8 md:gap-16">
-        <motion.div {...motionImageProps} className="relative w-full md:w-1/2 h-auto">
+        <motion.div {...motionImageProps} className="relative w-full md:w-1/2 h-[400px] md:h-auto">
           <Image
             src="/image/gallery1123.jpg"
             alt="Gallery image"
@@ -23,11 +23,11 @@ export default function AboutComponent() {
 
         <div className="w-full md:w-1/2 flex flex-col gap-4">
           <motion.div {...motionTextProps} className="">
-            <p className="text-3xl font-semibold text-gray-800 lg:text-[2.5rem]" style={{ lineHeight: '3rem' }}>Designing Your Dream With Brilliance</p>
+            <h2 className="text-2xl font-semibold text-gray-800 lg:text-[2.5rem]" style={{lineHeight: '3rem'}}>Designing Your Dream With Brilliance</h2>
           </motion.div>
 
           <div className="my-8 flex flex-col gap-4 justify-start text-left md:items-start">
-            <motion.p {...motionTextProps} className="px-12 pb-4 md:px-0">
+            <motion.p {...motionTextProps} className="pb-4 text-center md:text-left">
               Elevate your spaces with Addis Interiors interior designs that reflect your unique style and aspirations, crafted with precision and brilliance for an unforgettable living experience.
             </motion.p>
             <motion.div {...motionTextProps} className="flex px-12 gap-x-4 md:px-0">
@@ -36,7 +36,7 @@ export default function AboutComponent() {
             </motion.div>
           </div>
 
-          <motion.div {...motionBoxProps} className="">
+          <motion.div {...motionBoxProps} className="mx-auto md:mx-0">
             <Button href={'/contact'} title={'Contact Us'} />
           </motion.div>
         </div>
--- components/hero/ContactSection.jsx
+++ components/hero/ContactSection.jsx
@@ -1,15 +1,22 @@
+"use client"
+
 import Button from "../Button";
+import { motion } from "framer-motion";
+import { motionBoxProps, motionTextProps } from "@/lib/motionLib";
 
 export default function ContactSection() {
   return (
-    <div className="bg-zinc-100">
+    <motion.div {...motionBoxProps} className="bg-zinc-100">
       <div className="container items-center text-center lg:justify-between lg:flex py-16">
         <div className="py-4">
           <p className="text-2xl">Let's</p>
           <h1 className="text-3xl font-bold lg:text-5xl">Get in touch</h1>
         </div>
-        <Button href={'/contact'} title={'Contact Us'} />
+
+        <motion.div {...motionBoxProps}>
+          <Button href={'/contact'} title={'Contact Us'} />
+        </motion.div>
       </div>
-    </div>
+    </motion.div>
   )
 }
